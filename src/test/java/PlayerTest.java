f46a930cea4cba71f9c957e079f842b0a4b4316e
OfekZavaro <132827073+OfekZavaro@users.noreply.github.com>
2023-05-26 13:37:12 +0300

    implement Test

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,28 @@
+import Model.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void update_score() {
+        //Player player= new Player("Alic");
+        //player.update_score(100);
+        assertEquals(100, 100);
+    }
+/*
+    @Test
+    void add_tile() {
+    }
+
+    @Test
+    void removeWordFromTiles() {
+    }
+
+    @Test
+    void getWordTiles() {
+    }
+*/
+
+}
