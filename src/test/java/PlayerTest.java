23dea9d6e96813067459815f6d1068245b929aa5
OfekZavaro <132827073+OfekZavaro@users.noreply.github.com>
2023-05-26 16:01:31 +0300

    implement Test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,28 +1,58 @@
 import Model.Player;
-import org.junit.jupiter.api.Test;
+import ServerSide.Tile;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 class PlayerTest {
 
-    @Test
-    void update_score() {
-        //Player player= new Player("Alic");
-        //player.update_score(100);
-        assertEquals(100, 100);
-    }
-/*
-    @Test
-    void add_tile() {
+    public static void update_score() {
+        Player player= new Player("Alice");
+        player.update_score(100);
+        if(player.getScore() != 100){
+            System.out.println("Failed");
+        }
     }
 
-    @Test
-    void removeWordFromTiles() {
+    public static void add_tile() {
+        Player player= new Player("Alice");
+        player.add_tile();
+        if(player.getPlayer_tiles().isEmpty()){
+            System.out.println("Failed");
+        }
     }
 
-    @Test
-    void getWordTiles() {
-    }
-*/
+    public static void removeWordFromTiles() {
+        Player player= new Player("Alice");
+        String word= "AB";
+        player.getPlayer_tiles().add(Tile.Bag.getBag().getTile('A'));
+        player.getPlayer_tiles().add(Tile.Bag.getBag().getTile('B'));
+        player.removeWordFromTiles(word);
+        if(!player.getPlayer_tiles().isEmpty()){
+            System.out.println("Failed");
+        }
 
+    }
+
+
+    public static void getWordTiles() {
+        String word= "AB";
+        Tile tile1= Tile.Bag.getBag().getTile('A');
+        Tile tile2= Tile.Bag.getBag().getTile('B');
+        Player player= new Player("Alic");
+        List<Tile> tiles = new ArrayList<>();
+        tiles= player.getWordTiles(word);
+        if(!(tiles.contains(tile1) && tiles.contains(tile2))){
+            System.out.println("Failed");
+        }
+    }
+
+    public static void main(String[] args) {
+        update_score();
+        add_tile();
+        removeWordFromTiles();
+        getWordTiles();
+        System.out.println("testPlayer-done");
+    }
 }
